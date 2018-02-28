cb7a6c8e6fc620be02077428e9d0b53c4d77567b
Tsgegab <31626608+Tsgegab@users.noreply.github.com>
2018-02-28 00:56:43 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+        int count;
+
+    public static void main(String[] args) {
+
+    }
+
+    public int getCount() {
+        return count;
+    }
+int n =10;
+    public static int factorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++)
+            result *= i;
+        return result;
+    }
+}
